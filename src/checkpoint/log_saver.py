8a9f8a8bc4aeb297aaa45d443a783778b95365f4
Kornidesz Máté <dev.profile.km@gmail.com>
2026-05-21 00:12:54 +0200

    log_saver error solving

--- src/checkpoint/log_saver.py
+++ src/checkpoint/log_saver.py
@@ -4,6 +4,12 @@ import datetime
 from omegaconf import OmegaConf
 
 def save_logs(log_dir: str, save_path: str):
+    if not os.path.exists(log_dir):
+        os.makedirs(os.path.dirname(log_dir), exist_ok=True)   
+    
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
     with zipfile.ZipFile(save_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(log_dir):
             for file in files:
@@ -14,7 +20,7 @@ def main():
     config = OmegaConf.load("actions_config.yaml")
     LOG_DIRECTORY = config.actions.save.log_path
     SAVE_PATH = config.actions.save.save_path
-    FILE_NAME = f"logs_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.zip"
+    FILE_NAME = f"logs_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.zip"
     save_logs(
         LOG_DIRECTORY,
         os.path.join(SAVE_PATH, FILE_NAME)
